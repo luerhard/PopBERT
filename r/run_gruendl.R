3455597139b0ac541fbfdb27aadd7aa9d791a7a1
Lukas Erhard <luerhard@googlemail.com>
2023-08-21 16:07:17 +0200

    initial commit

---
+++ r/run_gruendl.R
@@ -0,0 +1,36 @@
+if (!is.element("pacman", installed.packages()[,1])) {
+  install.packages("pacman")
+}
+
+library("pacman")
+
+pacman::p_load("quanteda", "tidyverse", "optparse", "arrow")
+pacman::p_load_gh("jogrue/regexhelpeR", "jogrue/multidictR", "jogrue/popdictR")
+
+option_list <- list(
+  make_option(c("-f", "--file"), type="character", default=NULL,
+              help="dataset file (as parquet)", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default=NULL,
+              help="output file name ", metavar="character")
+)
+
+opt_parser <- OptionParser(option_list=option_list)
+opt <- parse_args(opt_parser)
+
+
+df <- arrow::read_parquet('/mnt/nvme_storage/git/bert_populism/tmp/gruendl/raw_sents.parquet')
+
+corpus <- quanteda::corpus(df, text_field="sentence")
+
+print("start popdictR...")
+
+result <- popdictR::run_popdict(corpus, return_value="binary")
+
+gruendl_result <- docvars(result) %>%
+  select(-c(n_tokens, n_sentences))
+
+print("saving output...")
+
+arrow::write_parquet(gruendl_result, opt$out)
+
+print("done.")
---
+++ setup.py
@@ -0,0 +1,4 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name="src", packages=find_packages())
