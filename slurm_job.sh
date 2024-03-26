202a252e827ec44b314b61398d18c6a452cd2422
Lukas Erhard <luerhard@googlemail.com>
2024-03-26 09:00:37 +0100

    add optional HPC slurm job

---
+++ slurm_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=dvc repro
+#SBATCH --partition=single
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --gres=gpu:1
+#SBATCH --time=08:00:00
+#SBATCH --mem=25gb
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=lukas.erhard@sowi.uni-stuttgart.de
+
+module load lib/cudnn/8.5.0-cuda-11.6
+
+bash run_all.sh
