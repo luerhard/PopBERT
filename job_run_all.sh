169f81fbefb3fd4c983fb8acdd2f297139fa3683
Lukas Erhard <luerhard@googlemail.com>
2024-03-27 23:00:04 +0100

    rerun predictions

--- job_run_all.sh
+++ job_run_all.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=10
 #SBATCH --gres=gpu:1
-#SBATCH --time=08:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mem=25gb
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=lukas.erhard@sowi.uni-stuttgart.de
